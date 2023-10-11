198e2f3e8b524f5242b5360d799988f7cbe06b61
Alan Lujan <alanlujan91@gmail.com>
2023-10-11 10:18:12 -0400

    clean up meta files

--- code/clean_md.py
+++ code/clean_md.py
@@ -8,10 +8,12 @@ for md_file in md_files:
     with open(md_file) as f:
         content = f.read()
 
-    # The regular expression ([a-zA-Z\.])\n([a-zA-Z\.]) matches any letter (a-z or A-Z) or a period,
+    # The regular expression ([a-zA-Z\.])\n([a-zA-Z\.])
+    # matches any letter (a-z or A-Z) or a period,
     # followed by a newline, and then another letter or a period.
     # The replacement string \1 \2 refers to the matched characters.
-    # So this line replaces a newline between a letter or a period with a space.
+    # So this line replaces a newline between a letter or a period with a
+    # space.
     new_content = re.sub(r"([a-zA-Z\.])\n([a-zA-Z\.])", r"\1 \2", content)
 
     # Write the modified content back to the file
